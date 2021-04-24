f6493cd0d0cfd0b7814d3023262653f297eeb666
Anand Krishna <anandkrishna2312@gmail.com>
2021-04-24 17:48:27 +0400

    backend: setup jinja2

--- heatmaps/server.py
+++ heatmaps/server.py
@@ -4,32 +4,38 @@ from asyncio import BaseEventLoop
 
 from aiohttp import ClientSession
 from dotenv import find_dotenv, load_dotenv
+from jinja2 import Environment, PackageLoader, select_autoescape
 from loguru import logger
 from motor.motor_asyncio import AsyncIOMotorClient
 from sanic import Sanic
 from sanic.request import Request
-from sanic.response import json, HTTPResponse
+from sanic.response import html, HTTPResponse
+
+from heatmaps.utils import render_page
 
 load_dotenv(find_dotenv())
 
 
 app = Sanic("heatmaps")
 
+# initialize database client
+app.ctx.db = AsyncIOMotorClient(os.environ.get("DATABASE_URL"))["heatmaps-db"]
+
+# initializing jinja2 templates
+app.ctx.env = Environment(
+    loader=PackageLoader("heatmaps", "templates"),
+    autoescape=select_autoescape(["html"]),
+    enable_async=True,
+)
+
 
 @app.listener("before_server_start")
 async def before_server_start(app: Sanic, loop: BaseEventLoop) -> None:
-    app.ctx.db = AsyncIOMotorClient(os.environ.get("DATABASE_URL"))["heatmaps-db"]
-    logger.info("Database client made")
     # create capped collection to store the data in case it isn't made already
     await app.ctx.db.create_collection("api_responses", capped=True, size=1000)
     app.ctx.http_session = ClientSession(loop=loop)
     logger.info("HTTP ClientSession made")
     # TODO: start data collection routine here. look into: sanic background tasks.
-
-
-@app.listener("after_server_start")
-async def after_server_start(app: Sanic, loop: BaseEventLoop) -> None:
-    logger.info("Heatmaps running!")
 
 
 @app.listener("before_server_stop")
@@ -40,5 +46,5 @@ async def close_db(app: Sanic, loop: BaseEventLoop) -> None:
 
 @app.route("/")
 async def index(request: Request) -> HTTPResponse:
-    # TODO: change this
-    return json({"hello": "world"})
+    output = await render_page(app.ctx.env, file="index.html")
+    return html(output)
--- heatmaps/utils.py
+++ heatmaps/utils.py
@@ -1,8 +1,16 @@
 """Utility functions and classes."""
 import asyncio
-from typing import Callable
+from typing import Any, Callable
 
 from loguru import logger
+from jinja2 import Environment
+
+
+async def render_page(environment: Environment, *, file: str, **context: Any) -> str:
+    """Helper function to render the template.
+    Use to give final output in the route functions."""
+    template = environment.get_template(file)
+    return await template.render_async(**context)
 
 
 class Loop:
