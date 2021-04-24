f9f5acc891208228d0f4c986aa61628d2533f919
Anand Krishna <anandkrishna2312@gmail.com>
2021-04-24 17:42:21 +0400

    backend: create background loop utils

---
+++ heatmaps/utils.py
@@ -0,0 +1,55 @@
+"""Utility functions and classes."""
+import asyncio
+from typing import Callable
+
+from loguru import logger
+
+
+class Loop:
+    """Helper class used for background task loops."""
+
+    def __init__(
+        self, func: Callable, *, seconds: int, minutes: int = 0, hours: int = 0
+    ) -> None:
+        self._func = func
+        self.interval = seconds
+        self.interval += minutes * 60
+        self.interval += hours * 3600
+
+        self._exc_count = (
+            0
+        )  # count exceptions as they occur; if more than 5 in a row; break the loop
+
+    async def _loop(self) -> None:
+        while True:
+            try:
+                await self._func()
+            except Exception as e:
+                logger.error(f"<TASK LOOP> Exception: {e}")
+                if self._exc_count > 5:
+                    logger.warning(
+                        f"<TASK LOOP> Broke the loop as it errored the last 5 runs"
+                    )
+                    break
+                self._exc_count += 1
+            else:
+                self._exc_count = 0
+                await asyncio.sleep(self.interval)
+
+    async def start(self) -> None:
+        """
+        Starts the underlying loop.
+        """
+        logger.info("<TASK LOOP> Starting Task {self._func.__name__}")
+        await self._loop()
+
+
+def loop(*, seconds: int, minutes: int = 0, hours: int = 0):
+    """
+    Decorator to register a coroutine as a loop.
+    """
+
+    def decorator(func: Callable):
+        return Loop(func, seconds=seconds, minutes=minutes, hours=hours)
+
+    return decorator
