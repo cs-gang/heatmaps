a91692b7956f3e0e2b9141e028ff6bf800d2a72c
Shriram Sekar <moonlight.shriram@gmail.com>
2021-05-11 09:07:51 +0400

    frontend: fix map minzoom and maxzoom, align map with page

--- heatmaps/templates/map.js
+++ heatmaps/templates/map.js
@@ -3,6 +3,8 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    zoom: 2,
+    minZoom: 2,
+    maxZoom: 4
   });
 }
